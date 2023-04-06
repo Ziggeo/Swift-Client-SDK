11619dab301db9c736ccb43e9d96b1f1ff6e1817
Severyn <severyn@ziggeo.com>
2023-04-06 21:19:01 +0800

    use modulemap

---
+++ Frameworks/ZiggeoMediaSwiftSDK.framework/Headers/ZiggeoMediaSwiftSDK-umbrella.h
@@ -0,0 +1,19 @@
+//
+//  ZiggeoMediaSwiftSDK-umbrella.h
+//  ZiggeoMediaSwiftSDK
+//
+//  Created by alex on 15/08/16.
+//  Copyright © 2016 Ziggeo Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ZiggeoMediaSwiftSDK.
+FOUNDATION_EXPORT double ZiggeoMediaSwiftSDKVersionNumber;
+
+//! Project version string for ZiggeoMediaSwiftSDK.
+FOUNDATION_EXPORT const unsigned char ZiggeoMediaSwiftSDKVersionString[];
+
+
+// In this header, you should import all the public headers of your framework using statements like #import 
+#import <ZiggeoMediaSwiftSDK/ZiggeoBFRImageViewController.h>
