b799781874fa11a03c8c17d9ee17c96073914aa0
Severyn <severyn@ziggeo.com>
2024-05-10 23:50:57 +0300

    ZIG-4992 - Raise minimum iOS deployment target to 15.0
    Raise minimum TestApplication iOS deployment target to 15.0

--- Examples/TestApplication/TestApplication/AppDelegate.swift
+++ Examples/TestApplication/TestApplication/AppDelegate.swift
@@ -14,31 +14,11 @@ import UserNotifications
 final class AppDelegate: UIResponder, UIApplicationDelegate {
     
     var window: UIWindow?
-
-
-    internal func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
-        do {
-            try AVAudioSession.sharedInstance().setCategory(AVAudioSession.Category.playAndRecord, options: [AVAudioSession.CategoryOptions.duckOthers, AVAudioSession.CategoryOptions.defaultToSpeaker]);
-        } catch {
-        }
-
-        if #available(iOS 10.0, *) {
-            let center  = UNUserNotificationCenter.current()
-            center.delegate = self
-            center.requestAuthorization(options: [.sound, .alert, .badge]) { (granted, error) in
-                if error == nil {
-                    DispatchQueue.main.async(execute: {
-                        UIApplication.shared.registerForRemoteNotifications()
-                    })
-                }
-            }
-        } else {
-            let settings = UIUserNotificationSettings(types: [.sound, .alert, .badge], categories: nil)
-            UIApplication.shared.registerUserNotificationSettings(settings)
-            UIApplication.shared.registerForRemoteNotifications()
-        }
-
+    
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        try? AVAudioSession.sharedInstance().setCategory(.playAndRecord, options: [.duckOthers, .defaultToSpeaker])
+        
+        registerPushNotifications()
         return true
     }
     
@@ -52,10 +32,23 @@ final class AppDelegate: UIResponder, UIApplicationDelegate {
 // MARK: - UNUserNotificationCenterDelegate
 extension AppDelegate: UNUserNotificationCenterDelegate {
     func userNotificationCenter(_ center: UNUserNotificationCenter, willPresent notification: UNNotification, withCompletionHandler completionHandler: @escaping (UNNotificationPresentationOptions) -> Void) {
-        completionHandler([[.alert, .badge, .sound]])
+        completionHandler([.banner, .badge, .sound, .list])
     }
     
     func userNotificationCenter(_ center: UNUserNotificationCenter, didReceive response: UNNotificationResponse, withCompletionHandler completionHandler: @escaping () -> Void) {
         completionHandler()
+    }
+}
+
+// MARK: - Privates
+private extension AppDelegate {
+    func registerPushNotifications() {
+        let center = UNUserNotificationCenter.current()
+        center.delegate = self
+        center.requestAuthorization(options: [.sound, .alert, .badge]) { granted, error in
+            guard error == nil, granted else { return }
+            
+            DispatchQueue.main.async(execute: UIApplication.shared.registerForRemoteNotifications)
+        }
     }
 }
