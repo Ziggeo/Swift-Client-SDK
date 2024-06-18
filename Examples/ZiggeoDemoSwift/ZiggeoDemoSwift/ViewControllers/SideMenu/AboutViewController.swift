286f6b0ecae64f25fd30ffbace37e21e35344f14
Severyn <severyn@ziggeo.com>
2024-06-18 22:14:35 +0300

    VPSE-111 - Doesn't open email dialog if user hasn't configure any email on device
    Add email deep links in LSApplicationQueriesSchemes

--- Examples/ZiggeoDemoSwift/ZiggeoDemoSwift/ViewControllers/SideMenu/AboutViewController.swift
+++ Examples/ZiggeoDemoSwift/ZiggeoDemoSwift/ViewControllers/SideMenu/AboutViewController.swift
@@ -9,10 +9,12 @@ import UIKit
 import WebKit
 import MessageUI
 
-final class AboutViewController: UIViewController {
+final class AboutViewController: UIViewController, MailScreenInvoker {
     
     // MARK: - Outlets
     @IBOutlet private weak var webView: WKWebView!
+    
+    var presenterViewController: UIViewController { self }
     
     // MARK: - Lifecycle
     override func viewDidLoad() {
@@ -21,21 +23,12 @@ final class AboutViewController: UIViewController {
         let url = Bundle.main.url(forResource: "about", withExtension: "html")! // swiftlint:disable:this force_unwrapping
         webView.loadFileURL(url, allowingReadAccessTo: url)
     }
-    
-    // MARK: - Actions
-    private func sendEmail(_ recipient: String) {
-        guard MFMailComposeViewController.canSendMail() else { return }
-        let mail = MFMailComposeViewController()
-        mail.setToRecipients([recipient])
-        mail.mailComposeDelegate = self
-        present(mail, animated: true)
-    }
 }
 
 // MARK: - MFMailComposeViewControllerDelegate
 extension AboutViewController: MFMailComposeViewControllerDelegate {
     func mailComposeController(_ controller: MFMailComposeViewController, didFinishWith result: MFMailComposeResult, error: Error?) {
-        dismiss(animated: true)
+        controller.dismiss(animated: true)
     }
 }
 
@@ -46,7 +39,7 @@ extension AboutViewController: WKNavigationDelegate {
         case .linkActivated:
             let link = navigationAction.request.url?.absoluteString ?? ""
             if link.contains("mailto:") {
-                sendEmail(link.replacingOccurrences(of: "mailto:", with: ""))
+                presentMailScreen(to: link.replacingOccurrences(of: "mailto:", with: ""))
             } else {
                 UIApplication.shared.openWebBrowser(link)
             }
